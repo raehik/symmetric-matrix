418eba0d74bc98b30cd4c6f5d250bdd3720d998c
Dominic Orchard <dom.orchard@gmail.com>
2022-09-14 12:05:09 +0100

    richer tests and fix reverse range bug

--- tests/test_constructors.py
+++ tests/test_constructors.py
@@ -3,5 +3,10 @@ from smatrix import smatrix
 def test_constructors():
   # All these constructors show succeed
   empty = smatrix.zeros(0)
+  assert empty.data == []
+
   singleton = smatrix.zeros(1)
+  assert singleton.data == [[0]]
+  
   two = smatrix.zeros(2)
+  assert two.data == [[0, 0], [0]]
